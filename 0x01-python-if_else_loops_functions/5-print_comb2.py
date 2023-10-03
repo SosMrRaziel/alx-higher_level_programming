b2a742a57af31d6d8a18c112a1d33a5671d3eaad
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-03 19:05:03 +0100

    pushhhh

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for index in range(0, 100):
     if index == 99:
-       print(f"{index:d}")
+       print("{}".format(index))
     else:
-       print(f"{index:02}", end=", ")
+       print("{:02}".format(index), end=", ")
