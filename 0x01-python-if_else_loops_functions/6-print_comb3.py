43db30f2d926cd266565115388242b5b085e5737
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-03 19:57:31 +0100

    print comp

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -4,5 +4,5 @@ for index in range(10):
         if index == 8 and jndix == 9:
             print("{}{}".format(index, jndix))
         else:
-            print(f"{}{}".format(index, jndix), end=", ")
+            print("{}{}".format(index, jndix), end=", ")
 
