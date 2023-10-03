9f12561dc6c1b605ffba88b21c09eacd8c5e649a
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-03 20:55:36 +0100

    uppercase

---
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def uppercase(str):
+    for index in str:
+        if ord(index) >= 97 and ord(index) <= 122:
+            index = chr(ord(index) - 32)
+        print(f"{index}", end="")
+    print("")
+
