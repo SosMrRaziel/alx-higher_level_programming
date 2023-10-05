f4dc137e937a87f8c3fed9c2de2d25db585467b9
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-05 19:38:18 +0100

    be

--- 0x02-python-import_modules/101-easy_print.py
+++ 0x02-python-import_modules/101-easy_print.py
@@ -1,4 +1,2 @@
 #!/usr/bin/python3
-if __name__ == '__main__':
-    from lazy import lazier as more_lazier
-more_lazier()
+from lazy import more_lazier
--- 0x02-python-import_modules/lazy.py
+++ 0x02-python-import_modules/lazy.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python3
 def lazier():
     print("#pythoniscool")
+more_lazier = lazier()
