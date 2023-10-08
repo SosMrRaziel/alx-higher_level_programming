9a187eb1b63911e80a5ba9cb4bf4febca7e2b230
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-08 18:42:50 +0100

    task

--- 0x03-python-data_structures/5-no_c.py
+++ 0x03-python-data_structures/5-no_c.py
@@ -1,7 +1,16 @@
+# #!/usr/bin/python3
+# def no_c(my_string):
+#     new_string = ""
+#     for char in my_string:
+#         if char not in "cC":
+#             new_string += char
+#     return new_string
+
+
 #!/usr/bin/python3
 def no_c(my_string):
     new_string = ""
     for char in my_string:
-        if char not in "cC":
+        if char != in "cC":
             new_string += char
     return new_string
