5dd7ff25856b4a118817d902db406e4f21d06e95
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-08 12:37:37 +0100

    lists

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def print_matrix_integer(matrix):
+def print_matrix_integer(matrix = ''):
     if bool(matrix) == 1:
         for i in matrix:
             print(*i)
     else:
-        print("")
+        print(matrix)
