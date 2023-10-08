6ace3b213d2ae40ef78d76efd0639b256ec1beda
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-08 15:49:48 +0100

    tuples

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,15 @@
+# #!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    result = (0, 0)
+    for i in range(2):
+        if i < len(tuple_a):
+            a = tuple_a[i]
+        else:
+            a = 0
+        if i < len(tuple_b):
+            b = tuple_b[i]
+        else:
+            b = 0
+        result = result[:i] + (a + b,) + result[i+1:]
+    return result
