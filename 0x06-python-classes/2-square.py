21a998cac936b69cc975e5bef496c0a61c141370
SosMrRaziel <lbt_alh@hotmail.fr>
2023-10-24 18:29:42 +0100

    task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,6 +1,12 @@
 #!/usr/bin/python3
+'''cass square'''
+
+
 class Square:
+    """A class that represents a square"""
+
     def __init__(self, size=0):
+        """Initialize the square with a given size"""
         try:
             self.__size = int(size)
         except ValueError:
