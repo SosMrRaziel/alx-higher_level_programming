1762d4fe69dc9088471a66e8879f9c58a93b2267
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-01 22:10:23 +0100

    task 6

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -7,47 +7,50 @@ max_integer = __import__('6-max_integer').max_integer
 
 
 class TestMaxInteger(unittest.TestCase):
-    """This is a test class of methods running
-    individual unittests."""
-    def test_one(self):
-        matrix1 = [1, 2, 3, 4]
-        self.assertEqual(max_integer(matrix1), 4)
+    """This is a test class of methods running individual unittests.
+    """
+    def test_postive(self):
+        """Test if max_integer handles positive max.
+        """
+        _list = [-1, 0, 1, 2]
+        self.assertEqual(max_integer(_list), 2)
 
-    def test_two(self):
-        matrix2 = []
-        self.assertEqual(max_integer(matrix2), None)
+    def test_negative(self):
+        """Test if max_integer handles negative max.
+        """
+        _list = [-9, -18, -27, -36]
+        self.assertEqual(max_integer(_list), -9)
 
-    def test_three(self):
-        self.assertRaises(TypeError, max_integer, 0)
+    def test_empty(self):
+        """Test if max_integer handles an empty list.
+        """
+        _list = []
+        self.assertIsNone(max_integer(_list))
 
-    def test_four(self):
-        matrix4 = {1: 'a', 2: 'b', 3: 'c'}
-        self.assertRaises(KeyError, max_integer, matrix4)
+    def test_one_elem(self):
+        """Test if max_integer handles one item list.
+        """
+        _list = [9]
+        self.assertEqual(max_integer(_list), 9)
 
-    def test_five(self):
-        matrix = ['m', 'r', 'k']
-        self.assertEqual(max_integer(matrix), 'r')
+    def test_str(self):
+        """Test if max_integer handles str comparison.
+        """
+        _list = ['Adam', 'Eve']
+        self.assertEqual(max_integer(_list), 'Eve')
 
-    def test_six(self):
-        matrix = 'raziel'
-        self.assertEqual(max_integer(matrix), 'z')
+    def test_chr(self):
+        """Test if max_integer handles chr comparison.
+        """
+        _list = ['a', 'b', 'c']
+        self.assertEqual(max_integer(_list), 'c')
 
-    def test_seven(self):
-        matrix = [-80, -81, -92, -11]
-        self.assertEqual(max_integer(matrix), -11)
-
-    def test_eight(self):
-        matrix1 = [5]
-        self.assertEqual(max_integer(matrix1), 5)
-
-    def test_ten(self):
-        matrix1 = [3, 3, 3, 3, 3]
-        self.assertEqual(max_integer(matrix1), 3)
-
-    def test_ten(self):
-        matrix1 = [3.2, 5.4, -8.6, 0.4, 17.5]
-        self.assertEqual(max_integer(matrix1), 17.5)
+    def test_mid_val(self):
+        """Test if max_integer handles middle val as max
+        """
+        _list = [0, 1, 4, 2, 3]
+        self.assertEqual(max_integer(_list), 4)
 
 
-if __name__ == '__main__':
+if __name__ is '__main__':
     unittest.main()
