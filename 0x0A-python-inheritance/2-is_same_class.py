f2c74d3cfed42cbced6989b8223ff179f619811e
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-06 19:42:14 +0100

    task 2

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+''' is_same_class'''
+
+
+def is_same_class(obj, a_class):
+    ''' Use the built-in type function to compare
+        the object's type with the class'''
+    return isinstance(obj, a_class)
