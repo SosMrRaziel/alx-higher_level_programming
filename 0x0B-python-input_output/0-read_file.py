257de0acf88c190588ef3cbb65cd27efdc8db632
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-07 16:50:22 +0100

    task 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+'''read_file function that reads a text file (UTF8)
+    and prints it to stdout'''
+
+
 def read_file(filename=""):
     '''Use the with statement to open the file and ensure it is closed'''
     with open(filename, encoding="utf-8") as f:
