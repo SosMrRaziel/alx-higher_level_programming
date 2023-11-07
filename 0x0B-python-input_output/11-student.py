bff52f3019e42d5a2fb657cab1ca84e170b4daa7
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-07 21:18:51 +0100

    TASK 11

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+'''Define a class Student'''
+
+
+class Student:
+    """A class that represents a student"""
+
+    def __init__(self, first_name, last_name, age):
+        """Initialize the student with first name, last name and age"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Return a dictionary representation of the student"""
+        if attrs is None:
+            # Return all attributes
+            return self.__dict__
+        else:
+            # Return only the specified attributes
+            return {k: v for k, v in self.__dict__.items() if k in attrs}
+
+
+    def reload_from_json(self, json):
+        """Replace all attributes of the student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)
