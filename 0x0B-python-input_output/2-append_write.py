83269266a8c711cb1727e7747683c6309193ec74
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-07 17:19:40 +0100

    task 2

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+'''append_write a function that appends a string at the end of a text file (UTF8)
+and returns the number of characters added'''
+
+
+def append_write(filename="", text=""):
+    '''open the file in append mode with UTF-8 encoding'''
+    with open(filename, "a", encoding="utf-8") as f:
+        return f.write(text)
