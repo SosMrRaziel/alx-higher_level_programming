944f9b32bba56b64d3015d1d5794982e2ae30a58
SosMrRaziel <lbt_alh@hotmail.fr>
2023-11-07 23:52:36 +0100

    task100

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Defines a text file insertion function."""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    """Appends a line of text to a file, after each line containing a specific string."""
+    # Open the file in read mode and store its contents in a list
+    with open(filename, "r") as f:
+        lines = f.readlines()
+    
+    # Create a new list to store the modified lines
+    new_lines = []
+    
+    # Loop through each line in the original file
+    for line in lines:
+        # Add the line to the new list
+        new_lines.append(line)
+        # If the line contains the search string, add the new string after it
+        if search_string in line:
+            new_lines.append(new_string + "\n")
+    
+    # Open the file in write mode and overwrite its contents with the new list
+    with open(filename, "w") as f:
+        f.writelines(new_lines)
