c11f7334cf2146609706d1e4f560340505f97b8b
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-08 21:30:34 +0100

    task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,9 +2,9 @@
 
 const args = process.argv.slice(2).map(Number);
 
-if (args.length === 0 || args === 1) {
-  console.log(0);
+if (args.length === 0 || args.length === 1){
+    console.log(0);
 } else {
-  args.sort((a, b) => b - a);
-  console.log(args[1]);
+        args.sort((a, b) => b - a);
+        console.log(args[1]);
 }
