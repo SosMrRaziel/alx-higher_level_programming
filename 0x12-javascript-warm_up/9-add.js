346cd9cc3d2e72382cf5fc43a202a2da23d9e458
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-08 20:30:27 +0100

    task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
 
 function add (a, b) {
-  const result = a + b;
-  return result;
+  return a + b;
 }
-const num1 = process.argv[2];
-const num2 = process.argv[3];
+
+const num1 = parseInt(process.argv[2]);
+const num2 = parseInt(process.argv[3]);
 const sum = add(num1, num2);
 console.log(sum);
+
+
