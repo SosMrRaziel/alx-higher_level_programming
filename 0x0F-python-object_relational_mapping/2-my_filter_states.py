bcb3c218ee8cbc7dec954d42108b9a9b49117556
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-12 17:49:27 +0100

    task 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+script that lists all states
+from the database hbtn_0e_0_usa
+"""
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    state_name_searched = sys.argv[4]
+
+    mydb = MySQLdb.connect(host="localhost",
+                           port=3306,
+                           user=username,
+                           password=password,
+                           db=database
+                           )
+    cur = mydb.cursor()
+    cur.execute("SELECT * FROM states WHERE name = %s\
+                ORDER BY id", (state_name_searched,))
+    rows = cur.fetchall()
+    for row in rows:
+        print(row)
+if mydb.open:
+    cur.close()
+    mydb.close()
