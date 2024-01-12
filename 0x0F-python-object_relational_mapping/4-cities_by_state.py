a03c289e949685ecb12035738c298792022e70eb
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-12 21:07:48 +0100

    task 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,5 +1,10 @@
-
-
+#!/usr/bin/python3
+"""
+A script that is safe from SQL injections
+and lists all states from the database hbtn_0e_0_usa.
+"""
+import MySQLdb
+import sys
 
 import MySQLdb
 import sys
@@ -16,7 +21,8 @@ if __name__ == "__main__":
                            db=database
                            )
     cur = mydb.cursor()
-    cur.execute("SELECT * FROM cities ORDER BY id ASC")
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN\
+                states ON cities.state_id = states.id ORDER BY cities.id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
