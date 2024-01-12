1ee862d88d66cec5f8a16000987868dd6f389b18
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-12 20:49:51 +0100

    task 3

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,8 +1,6 @@
-#!/usr/bin/python3
-"""
-A script that is safe from SQL injections
-and lists all states from the database hbtn_0e_0_usa.
-"""
+
+
+
 import MySQLdb
 import sys
 
@@ -10,7 +8,6 @@ if __name__ == "__main__":
     username = sys.argv[1]
     password = sys.argv[2]
     database = sys.argv[3]
-    state_name_searched = sys.argv[4]
 
     mydb = MySQLdb.connect(host="localhost",
                            port=3306,
@@ -19,8 +16,7 @@ if __name__ == "__main__":
                            db=database
                            )
     cur = mydb.cursor()
-    query = "SELECT * FROM states WHERE name = %s ORDER BY id ASC"
-    cur.execute(query, (state_name_searched,))
+    cur.execute("SELECT * FROM cities ORDER BY id ASC")
     rows = cur.fetchall()
     for row in rows:
         print(row)
