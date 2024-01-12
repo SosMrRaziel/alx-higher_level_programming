c6342c8a98a21b9f665a1b8ce43f6712cdb24825
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-12 20:26:19 +0100

    task 3

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-A script that is safe from SQL injections and lists all states from the database hbtn_0e_0_usa.
+A script that is safe from SQL injections 
+and lists all states from the database hbtn_0e_0_usa.
 """
 import MySQLdb
 import sys
