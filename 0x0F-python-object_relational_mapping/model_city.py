9e4431e9f6083c852e2baadbb13e2ede0f57347e
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-13 03:45:17 +0100

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,12 +1,11 @@
 #!/usr/bin/python3
-
-from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.ext.declarative import declarative_base
-from model_state import Base, State
 """
 Contains the class definition of a City.
 """
 
+from sqlalchemy import Column, Integer, String, ForeignKey
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
 
 class City(Base):
     """Attributes of the City class:
