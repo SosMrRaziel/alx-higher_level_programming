0c625c7322f2a47111cf7ee0e73456bfce7b63dc
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-13 03:47:48 +0100

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -2,12 +2,13 @@
 """
 Contains the class definition of a City.
 """
-
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
 
+
 class City(Base):
+
     """Attributes of the City class:
     id: An integer that serves as the unique identifier for each city.
     It is auto-generated, cannot be null,
@@ -19,6 +20,7 @@ class City(Base):
     key to the id attribute of the states table.
     It cannot be null and establishes a relationship
     between each city and its corresponding state."""
+
     __tablename__ = 'cities'
     id = Column(Integer, primary_key=True,
                 nullable=False, autoincrement="auto")
