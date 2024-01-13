2ba99aaf2d3697c79ac365c5f7dae5f56d825820
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-13 03:44:31 +0100

    task 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,25 +1,27 @@
-
-
 #!/usr/bin/python3
-"""
-Contains the class definition of a City.
-"""
 
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import Base, State
+"""
+Contains the class definition of a City.
+"""
+
 
 class City(Base):
     """Attributes of the City class:
-
-id: An integer that serves as the unique identifier for each city.
-It is auto-generated, cannot be null, and is set as the primary key of the table.
-name: A string of up to 128 characters that represents the name of the city.
-This attribute cannot be null.
-state_id: An integer that acts as a foreign key to the id attribute of the states table.
-It cannot be null and establishes a relationship
-between each city and its corresponding state."""
+    id: An integer that serves as the unique identifier for each city.
+    It is auto-generated, cannot be null,
+    and is set as the primary key of the table.
+    name: A string of up to 128 characters
+    that represents the name of the city.
+    This attribute cannot be null.
+    state_id: An integer that acts as a foreign
+    key to the id attribute of the states table.
+    It cannot be null and establishes a relationship
+    between each city and its corresponding state."""
     __tablename__ = 'cities'
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement="auto")
+    id = Column(Integer, primary_key=True,
+                nullable=False, autoincrement="auto")
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
