853873dca25c4632f192a868cb964b63595efa66
SOSmeraziel <lbt_alh@hotmail.fr>
2024-01-25 10:34:14 -0800

    task1

--- 0x10-python-network_0/1-body.sh
+++ 0x10-python-network_0/1-body.sh
@@ -1,3 +1,4 @@
-#!bin/bash
-# a Bash script that takes in a URL, sends a GET request to the URL, and displays the body of the response
-crul -s -L "$1"
+#!/bin/bash
+# a Bash script that takes in a URL, sends a GET request to the URL
+curl -s -L "$1"
+
