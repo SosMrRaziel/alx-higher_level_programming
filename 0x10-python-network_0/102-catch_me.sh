20573f05eee318dedf1b3328949ee7f6383a7178
SOSmeraziel <lbt_alh@hotmail.fr>
2024-01-25 11:07:13 -0800

    task 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!
-curl -s -o /dev/null -w "%{http_code}" "$1"
+curl -o /dev/null -sw "You find me!" 0.0.0.0:5000/catch_me
