c493213e3cca0d11bdee4b1541010308e044e269
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-26 18:14:02 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -12,7 +12,7 @@ if __name__ == "__main___":
     with urllib.request.urlopen(url) as r:
         html1 = r.read()
         html2 = html1.decode('utf-8')
-        print("Body response:")
-        print("\t- type: {}".format(type(html1)))
-        print("\t- content: {}".format(html1))
-        print("\t- utf8 content: {}".format(html2))
+    print("Body response:")
+    print("\t- type: {}".format(type(html1)))
+    print("\t- content: {}".format(html1))
+    print("\t- utf8 content: {}".format(html2))
