fcb989ed2ef76b0d72a32697fa14a48985ec089c
sosmrraziel <lbt_alh@hotmail.fr>
2024-01-26 12:03:12 -0800

    task 6

--- 0x11-python-network_1/6-post_email.py
+++ 0x11-python-network_1/6-post_email.py
@@ -11,7 +11,8 @@ import sys
 if __name__ == "__main__":
 
     url = sys.argv[1]
-    data = {"email": sys.argv[2]}
+    email = sys.argv[2]
+    data = {"email": email}
 
     resp = requests.post(url, data)
     print(resp.text)
