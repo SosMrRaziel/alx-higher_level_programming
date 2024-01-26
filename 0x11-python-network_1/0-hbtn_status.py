56a60ad03c69a59717ef4f26e3d02d6dd09a1e0b
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-26 18:02:10 +0100

    task0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+#a Python script that fetches https://alx-intranet.hbtn.io/status
+import urllib.request
+
+url = "https://alx-intranet.hbtn.io/status"
+
+with urllib.request.urlopen(url) as r:
+    html1 = r.read()
+    html2 = html1.decode('utf-8')
+    print("Body response:$")
+    print("\t- type: {}".format(type(html1)))
+    print("\t- content: {}".format(html1))
+    print("\t- utf8 content: {}".format(html2))
