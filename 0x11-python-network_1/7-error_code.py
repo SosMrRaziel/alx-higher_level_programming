46708b4cc05d8145a9231db6532be00e11a72331
sosmrraziel <lbt_alh@hotmail.fr>
2024-01-26 12:23:42 -0800

    task 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -10,6 +10,6 @@ if __name__ == "__main__":
     url = sys.argv[1]
     resp = requests.get(url)
     if resp.status_code >= 400:
-       print("Error code:", resp.status_code)
+        print("Error code:", resp.status_code)
     else:
         print(resp.text)
