3d8fa8ffe46d1b94daebac89870f40964b80a40e
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-26 20:03:15 +0100

    task 3

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -2,7 +2,8 @@
 """a Python script that takes in a URL, sends a request to the URL
 and displays the body of the response (decoded in utf-8)."""
 
-import urllib.request, urllib.error 
+import urllib.request
+import urllib.error
 import sys
 
 if __name__ == "__main__":
