6e028ef7f992fbef72861c594c3016ced908a89a
SosMrRaziel <lbt_alh@hotmail.fr>
2024-01-26 18:17:02 +0100

    task 0

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
 """a Python script that fetches https://alx-intranet.hbtn.io/status"""
-
-
 import urllib.request
 
 
-if __name__ == "__main___":
+if __name__ == "__main__":
 
-    url = "https://alx-intranet.hbtn.io/status"
+	url = "https://alx-intranet.hbtn.io/status"
 
-    with urllib.request.urlopen(url) as r:
-        html1 = r.read()
-        html2 = html1.decode('utf-8')
-    print("Body response:")
-    print("\t- type: {}".format(type(html1)))
-    print("\t- content: {}".format(html1))
-    print("\t- utf8 content: {}".format(html2))
+	with urllib.request.urlopen(url) as r:
+		html1 = r.read()
+		html2 = html1.decode('utf-8')
+	print("Body response:")
+	print("\t- type: {}".format(type(html1)))
+	print("\t- content: {}".format(html1))
+	print("\t- utf8 content: {}".format(html2))
