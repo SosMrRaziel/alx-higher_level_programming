eb202dfc8ab85005bf341dba9d7e683e1a211642
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-20 22:27:16 +0100

    task

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -1,21 +1,20 @@
 #!/usr/bin/node
 
-const request = require ('request');
-const fs = require ('fs');
+const request = require('request');
+const fs = require('fs');
 
-const url = process.argv [2]; // The URL to request
-const file = process.argv [3]; // The file path to store the body response
+const url = process.argv[2];
+const file = process.argv[3];
 
-request.get (url, function (error, response, body) {
+request.get(url, function (error, response, body) {
   if (error) {
-    console.error (error);
+    console.error(error);
     return;
   }
   if (response.statusCode === 200) {
-    fs.writeFile (file, body, 'utf8', function (error) {
+    fs.writeFile(file, body, 'utf8', function (error) {
       if (error) {
-        console.error (error);
-        return;
+        console.error(error);
       }
     });
   }
