dc90410895cba184b70f05257daea429eac9f054
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-20 21:58:29 +0100

    task

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,25 +1,21 @@
 #!/usr/bin/node
-
-const request = require("request");
-const link = process.argv[2];
-
-request(link, function(error, response, body) {
-    if (error) {
-        console.error(error);
-    }
-    else {
-        const data = JSON.parse(body);
-        const results = data.results;
-        let counter = 0;
-        for (let i = 0; i < results.length; i++) {
-            const movie = results[i];
-            const charac = movie.characters;
-            if (charac.includes("https://swapi-api.alx-tools.com/api/films/people/18")) {
-                counter ++;
-            }
+const request = require('request');
+const url = process.argv[2];
+let data;
+let movies = 0;
+request(url, function (error, response, body) {
+  if (error) {
+    console.error(error);
+  } else {
+    data = JSON.parse(body);
+    data.results.forEach(function (result) {
+      result.characters.forEach(function (character) {
+        const split = character.split('/');
+        if (split[split.length - 2] === '18') {
+          movies++;
         }
-        console.log(counter)
-    }
-
-}
-)
+      });
+    });
+    console.log(movies);
+  }
+});
