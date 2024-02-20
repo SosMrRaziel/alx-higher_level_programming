5142f55d72a080f6179bf4aff4f7b2be26942dcc
SosMrRaziel <lbt_alh@hotmail.fr>
2024-02-20 18:27:07 +0100

    task

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-const fs = require('fs')
-const file_path = process.argv[2]
+const fs = require('fs');
+const filePath = process.argv[2];
 
-fs.readFile(file_path, 'utf8', function(err, data) {
-    if (err) {
-        console.error(err);
-    }
-    else {
-        console.log(data);
-    }
-})
+fs.readFile(filePath, 'utf8', function (err, data) {
+  if (err) {
+    console.error(err);
+  } else {
+    console.log(data);
+  }
+});
